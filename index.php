bf3378f74f43ccbbe8c70f132bdba1b9c18b6fd7
EltonGaleti113 <egaleti2003@gmail.com>
2024-08-15 14:04:48 -0300

    html table added

--- index.php
+++ index.php
@@ -1,22 +1,39 @@
-<?php 
-    require('php/action-task.php');
+<?php
+// require('php/action-task.php');
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task List</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class= 'bg-slate-600'>
+
+<body class='bg-slate-600'>
     <h1>Olá Bem-Vindo</h1>
     <!-- Formulário que recebe input $task -->
     <form action="php/action-task.php" method="post">
-        <label for="">Task:</label>
+        <label for="">Nome:</label>
         <input type="text" name="task" class="">
-    <!-- Submit -->
+        <!-- Submit -->
         <input type="submit" name="Submit">
     </form>
+    <div class="bg-slate-200">
+        <table>
+            <tr>
+                <th>Tarefas</th>
+            </tr>
+            <tr>
+                <td>Tarefa 1</td>
+            </tr>
+            <tr>
+                <td><button class="bg-yellow-500"><a href="#editar">Editar</a></button></td>
+                <td><button class="bg-red-600"><a href="#excluir">Excluir</a></button></td>
+            </tr>
+        </table>
+    </div>
 </body>
+
 </html>
--- php/action-task.php
+++ php/action-task.php
@@ -1,5 +1,7 @@
 <?php
+session_start();
 
+$task = [];
 $task = $_POST['task'];
 
 echo "hello ". htmlspecialchars($_POST['task']). '!'; 
